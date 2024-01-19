65d6d5965372186a2764ebd7882eba3974881134
Brilliant-Developer1 <email2obaidul@gmail.com>
2024-01-19 22:57:52 +0600

    Random Video urls added

--- scripts.js
+++ scripts.js
@@ -1,5 +1,31 @@
 const playButton = document.getElementsByClassName('player__button toggle')[0];
 const videoPlayer = document.querySelector('.player__video');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
+const sliders = document.querySelectorAll('.player__slider');
+const skipBtns = document.querySelectorAll('[data-skip]');
+
+const videos = [
+  'https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+  'https://storage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+  'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
+  'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
+  'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4',
+  'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4',
+  'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4',
+  'https://storage.googleapis.com/gtv-videos-bucket/sample/Sintel.jpg',
+  'https://storage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4',
+  'https://storage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4',
+];
+
+const getUrl = videos => {
+  const randomIndex = Math.floor(Math.random() * videos.length);
+  const url = videos[randomIndex];
+
+  return url;
+};
+
+videoPlayer.src = getUrl(videos);
 
 const handlePlay = () => {
   if (videoPlayer.paused || videoPlayer.ended) {
@@ -14,3 +40,44 @@ const handlePlay = () => {
 };
 
 playButton.addEventListener('click', handlePlay);
+videoPlayer.addEventListener('click', handlePlay);
+document.addEventListener('keydown', event => {
+  if (event.code === 'Space') {
+    event.preventDefault();
+    handlePlay();
+  }
+});
+
+function handleProgress() {
+  const progressPercentage =
+    (videoPlayer.currentTime / videoPlayer.duration) * 100;
+  progressBar.style.flexBasis = `${progressPercentage}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * videoPlayer.duration;
+  videoPlayer.currentTime = scrubTime;
+}
+
+videoPlayer.addEventListener('timeupdate', handleProgress);
+progress.addEventListener('click', scrub);
+let mousedown = false;
+progress.addEventListener('mousedown', () => (mousedown = true));
+progress.addEventListener('mousemove', e => mousedown && scrub(e));
+progress.addEventListener('mouseup', () => (mousedown = false));
+
+function handleSliderUpdate() {
+  videoPlayer[this.name] = this.value;
+}
+
+sliders.forEach(slider => {
+  slider.addEventListener('change', handleSliderUpdate);
+});
+
+function handleSkip() {
+  videoPlayer.currentTime += +this.dataset.skip;
+}
+
+skipBtns.forEach(btn => {
+  btn.addEventListener('click', handleSkip);
+});
